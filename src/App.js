10b297c38e547e749c014675c0517fd868ccd034
Li-Ling Chang <lchang@obsglobal.com>
2018-06-14 09:19:16 -0600

    Added portrait img and intro styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
         <Layout>
             <Intro/>
         </Layout>
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './Intro.css' ;
+import styles from './Intro.css' ;
+import portraitImg from '../../assets/images/portrait_sm_sep.jpg';
 
 const intro = (props) => {
   return (
-      <div className="Intro">
-          <h2>Hi, I'm Li-Ling Chang -</h2>
-          <h3>I am a web developer who is passionate about creating simple and unique user experience for everyday things.</h3>
+      <div className={styles.Intro}>
+        <div className={styles.IntroContent}>
+         <img src={portraitImg} alt="Me" />
+         <div>
+          <h1>Hi, my name is Li-Ling Chang.</h1>
+          <p className={styles.SubTitle}>pronounced as Lee Ling Chang</p>
+          <h3>I am a web developer who is passionate about creating simple and unique user experiences for everyday things.</h3>
+          </div>
+        </div>
       </div>
   )
 };
